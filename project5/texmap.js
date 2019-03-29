63a2143b58096750ff7f09f32c6b8ccc7e442ec8
scao7 <codescao7@gmail.com>
2019-03-28 21:21:37 -0500

    backup

--- project5/texmap.js
+++ project5/texmap.js
@@ -6,15 +6,15 @@ var gl;
 var numVertices  = 36;
 
 var texSize = 64;
-
+var on = true ;
 var program;
-
+var modelViewMatrix = rotate(0, 0,0,1);
 var pointsArray = [];
 var colorsArray = [];
 var texCoordsArray = [];
-
+var image = [];
 var texture;
-
+var change = 0;
 var texCoord = [
     vec2(0, 0),
     vec2(0, 1),
@@ -23,14 +23,14 @@ var texCoord = [
 ];
 
 var vertices = [
-    vec4( -0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5,  0.5,  0.5, 1.0 ),
-    vec4( 0.5,  0.5,  0.5, 1.0 ),
-    vec4( 0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5, -0.5, -0.5, 1.0 ),
-    vec4( -0.5,  0.5, -0.5, 1.0 ),
-    vec4( 0.5,  0.5, -0.5, 1.0 ),
-    vec4( 0.5, -0.5, -0.5, 1.0 )
+    vec4( -0.5, -0.5,  0.1, 1.0 ),
+    vec4( -0.5,  0.5,  0.1, 1.0 ),
+    vec4( 0.5,  0.5,  0.1, 1.0 ),
+    vec4( 0.5, -0.5,  0.1, 1.0 ),
+    vec4( -0.5, -0.5, -0.1, 1.0 ),
+    vec4( -0.5,  0.5, -0.1, 1.0 ),
+    vec4( 0.5,  0.5, -0.1, 1.0 ),
+    vec4( 0.5, -0.5, -0.1, 1.0 )
 ];
 
 var vertexColors = [
@@ -42,13 +42,13 @@ var vertexColors = [
     vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
     vec4( 0.0, 1.0, 1.0, 1.0 ),  // white
     vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
-];    
+];
 
 var xAxis = 0;
 var yAxis = 1;
 var zAxis = 2;
 var axis = xAxis;
-var theta = [45.0, 45.0, 45.0];
+var theta = [180, 0.0, 0.0];
 
 var thetaLoc;
 
@@ -56,43 +56,42 @@ function configureTexture( image ) {
     texture = gl.createTexture();
     gl.bindTexture( gl.TEXTURE_2D, texture );
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, 
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB,
          gl.RGB, gl.UNSIGNED_BYTE, image );
     gl.generateMipmap( gl.TEXTURE_2D );
-    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, 
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
                       gl.NEAREST_MIPMAP_LINEAR );
     gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
-    
+
     gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
 }
 
 
 function quad(a, b, c, d) {
-     pointsArray.push(vertices[a]); 
-     colorsArray.push(vertexColors[a]); 
+     pointsArray.push(vertices[a]);
+     colorsArray.push(vertexColors[2]);
      texCoordsArray.push(texCoord[0]);
 
-     pointsArray.push(vertices[b]); 
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[1]); 
+     pointsArray.push(vertices[b]);
+     colorsArray.push(vertexColors[2]);
+     texCoordsArray.push(texCoord[1]);
 
-     pointsArray.push(vertices[c]); 
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[2]); 
-   
-     pointsArray.push(vertices[a]); 
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[0]); 
+     pointsArray.push(vertices[c]);
+     colorsArray.push(vertexColors[2]);
+     texCoordsArray.push(texCoord[2]);
 
-     pointsArray.push(vertices[c]); 
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[2]); 
+     pointsArray.push(vertices[a]);
+     colorsArray.push(vertexColors[2]);
+     texCoordsArray.push(texCoord[0]);
 
-     pointsArray.push(vertices[d]); 
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[3]);   
+     pointsArray.push(vertices[c]);
+     colorsArray.push(vertexColors[2]);
+     texCoordsArray.push(texCoord[2]);
+
+     pointsArray.push(vertices[d]);
+     colorsArray.push(vertexColors[2]);
+     texCoordsArray.push(texCoord[3]);
 }
-
 
 function colorCube()
 {
@@ -103,18 +102,24 @@ function colorCube()
     quad( 4, 5, 6, 7 );
     quad( 5, 4, 0, 1 );
 }
+function printBase() {
 
+  modelViewMatrix = mult(modelViewMatrix, translate(-0.9, 0, 0));
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+  gl.drawArrays(gl.TRIANGLES, 0, 36);
+  // modelViewMatrix = mult(modelViewMatrix, rotate(20, 1, 0, 0));
+}
 
 window.onload = function init() {
 
     canvas = document.getElementById( "gl-canvas" );
-    
+
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    
+
     gl.enable(gl.DEPTH_TEST);
 
     //
@@ -122,13 +127,13 @@ window.onload = function init() {
     //
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
-    
+
     colorCube();
 
     var cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
-    
+
     var vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vColor );
@@ -136,15 +141,19 @@ window.onload = function init() {
     var vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
-    
+
+
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
-    
+
     var tBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW );
-    
+
     var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
     gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vTexCoord );
@@ -154,29 +163,73 @@ window.onload = function init() {
     //
 
     //var image = new Image();
-    //image.onload = function() { 
+    //image.onload = function() {
      //   configureTexture( image );
     //}
     //image.src = "SA2011_black.gif"
 
 
-    var image = document.getElementById("texImage");
- 
-    configureTexture( image );
 
-    thetaLoc = gl.getUniformLocation(program, "theta"); 
-    
+
+     image[0] = document.getElementById("texImage");
+     image[1] = document.getElementById("texImage1");
+     image[2] = document.getElementById("texImage2");
+     image[3] = document.getElementById("textImage3");
+
+     configureTexture(image[change]);
+
+
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
+
     document.getElementById("ButtonX").onclick = function(){axis = xAxis;};
     document.getElementById("ButtonY").onclick = function(){axis = yAxis;};
     document.getElementById("ButtonZ").onclick = function(){axis = zAxis;};
-       
-    render();
- 
+    document.getElementById("ButtonNext").onclick = function(){
+      change += 1;
+      if(change > 2 ){
+        change = 0;
+      }
+
+      configureTexture(image[change]);
+
+      console.log(change);
+       render();
+    };
+    document.getElementById("ButtonPrev").onclick = function(){
+      change -= 1;
+      if(change < 0 ){
+        change = 2;
+      }
+      configureTexture(image[change]);
+       render();
+    };
+    document.getElementById("ButtonOn").onclick = function(){
+        if(on == true){
+          on = false;
+        }
+        else{
+          on = true;
+        }
+        configureTexture(image[1]);
+        console.log(on);
+       render();
+    };
+ //printBase();
+render();
+
 }
+
 
 var render = function(){
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    theta[axis] += 2.0;
+    //theta[axis] += 2.0;
+    if(on == true){
+      configureTexture(image[change]);
+    }
+    else{
+      configureTexture(image[1]);
+    }
     gl.uniform3fv(thetaLoc, flatten(theta));
     gl.drawArrays( gl.TRIANGLES, 0, numVertices );
     requestAnimFrame(render);
