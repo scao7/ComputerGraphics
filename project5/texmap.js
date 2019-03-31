691f1150aee53989b264c81fce99597816654113
scao7 <scao7@crimson.ua.edu>
2019-03-30 20:04:21 -0500

    finished project5

--- project5/texmap.js
+++ project5/texmap.js
@@ -1,4 +1,5 @@
 
+// Shengting Cao CS435 project5
 
 var canvas;
 var gl;
@@ -8,7 +9,8 @@ var texSize = 64;
 var on = true;
 var pause = true;
 var program;
-var modelViewMatrix = rotate(30, -1, 0, 1);
+var modelViewMatrix = rotate(10, -1, 0, 0);
+modelViewMatrix = mult(rotate(10,0,1,0),modelViewMatrix);
 var pointsArray = [];
 var colorsArray = [];
 var texCoordsArray = [];
@@ -19,6 +21,12 @@ var image = [];
 var texture;
 var change = 0;
 
+
+var wallPaper;
+var holderPaper;
+var TVPaper;
+var floorPaper;
+var metalPaper;
 var mdvStatic = rotate(45, 1, 0, 0);
 var texCoord = [
   vec2(0, 0),
@@ -68,60 +76,62 @@ function configureTexture(image) {
     gl.NEAREST_MIPMAP_LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
-  gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+ // gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+
+  return texture;
 }
 
 
 function quad(a, b, c, d) {
   pointsArray.push(vertices[a]);
-  colorsArray.push(vertexColors[2]);
+  colorsArray.push(vertexColors[6]);
   texCoordsArray.push(texCoord[0]);
 
   pointsArray.push(vertices[b]);
-  colorsArray.push(vertexColors[2]);
+  colorsArray.push(vertexColors[6]);
   texCoordsArray.push(texCoord[1]);
 
   pointsArray.push(vertices[c]);
-  colorsArray.push(vertexColors[2]);
+  colorsArray.push(vertexColors[6]);
   texCoordsArray.push(texCoord[2]);
 
   pointsArray.push(vertices[a]);
-  colorsArray.push(vertexColors[2]);
+  colorsArray.push(vertexColors[6]);
   texCoordsArray.push(texCoord[0]);
 
   pointsArray.push(vertices[c]);
-  colorsArray.push(vertexColors[2]);
+  colorsArray.push(vertexColors[6]);
   texCoordsArray.push(texCoord[2]);
 
   pointsArray.push(vertices[d]);
-  colorsArray.push(vertexColors[2]);
+  colorsArray.push(vertexColors[6]);
   texCoordsArray.push(texCoord[3]);
 }
 
 function quadWithoutTex(a, b, c, d) {
   pointsArrayNoTex.push(vertices[a]);
   colorsArrayNoTex.push(vertexColors[2]);
-  //texCoordsArray.push(texCoord[0]);
+  texCoordsArray.push(texCoord[0]);
 
   pointsArrayNoTex.push(vertices[b]);
   colorsArrayNoTex.push(vertexColors[2]);
-  //texCoordsArray.push(texCoord[1]);
+  texCoordsArray.push(texCoord[1]);
 
   pointsArrayNoTex.push(vertices[c]);
   colorsArrayNoTex.push(vertexColors[2]);
-  //texCoordsArray.push(texCoord[2]);
+  texCoordsArray.push(texCoord[2]);
 
   pointsArrayNoTex.push(vertices[a]);
   colorsArrayNoTex.push(vertexColors[2]);
-  //texCoordsArray.push(texCoord[0]);
+  texCoordsArray.push(texCoord[0]);
 
   pointsArrayNoTex.push(vertices[c]);
   colorsArrayNoTex.push(vertexColors[2]);
- // texCoordsArray.push(texCoord[2]);
+  texCoordsArray.push(texCoord[2]);
 
   pointsArrayNoTex.push(vertices[d]);
   colorsArrayNoTex.push(vertexColors[2]);
- //texCoordsArray.push(texCoord[3]);
+  texCoordsArray.push(texCoord[3]);
 }
 
 function colorCube() {
@@ -161,7 +171,7 @@ window.onload = function init() {
   gl.useProgram(program);
 
   colorCube();
-
+  // colorCubeWithoutTex()
   //buffer for tex instance 
   var cBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
@@ -214,7 +224,15 @@ window.onload = function init() {
   image[1] = document.getElementById("texImage1");
   image[2] = document.getElementById("texImage2");
   image[3] = document.getElementById("texImage3");
-
+  image[4] = document.getElementById("texImage4");
+  image[5] = document.getElementById("texImage5");
+  image[6] = document.getElementById("texImage6");
+  image[7] = document.getElementById("texImage7");
+  wallPaper = configureTexture(image[5]);
+  holderPaper = configureTexture(image[4]);
+  TVPaper = configureTexture(image[0]);
+  floorPaper = configureTexture(image[6]);
+  metalPaper = configureTexture(image[7]);
   //configureTexture(image[change]);
   configureTexture(image[change]);
 
@@ -230,7 +248,7 @@ window.onload = function init() {
       change = 0;
     }
 
-    configureTexture(image[change]);
+    TVPaper = configureTexture(image[change]);
 
     render();
   };
@@ -241,18 +259,18 @@ window.onload = function init() {
       change = 2;
     }
     if (on)
-      configureTexture(image[change]);
+    TVPaper = configureTexture(image[change]);
     render();
   };
 
   document.getElementById("ButtonOn").onclick = function () {
     if (on == true) {
       on = false;
-      configureTexture(image[3]);
+      TVPaper = configureTexture(image[3]);
     }
     else {
       on = true;
-      configureTexture(image[change]);
+      TVPaper = configureTexture(image[change]);
     }
   };
 
@@ -284,9 +302,9 @@ var render2 = function () {
     change = 0;
   }
   if (on == false) {
-    configureTexture[image[3]];
+    TVPaper = configureTexture[image[3]];
   }
-  else configureTexture(image[change]);
+  else TVPaper = configureTexture(image[change]);
   render();
 }
 
@@ -294,36 +312,43 @@ var render = function () {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   //theta[axis] += 2.0;
 
-  printBase();
-  drawTV();
   // printBase();
+  drawTV();
   leftWall();
   rightWall();
   backWall();
   holder();
+  backBoard();
+  floor();
+  table();
+  tableLeg1();
+  tableLeg2();
+  neck();
   requestAnimFrame(render);
 }
 
 function drawTV() {
-  var s = scalem(0.5, 0.5, 0.5);
-  var instanceMatrix = mult(translate(0.0, 0.0, 0.0), s);
+  var s = scalem(0.8, 0.55, 0.8);
+  var instanceMatrix = mult(translate(0.0, 0.32, 0.1), s);
   var t = mult(modelViewMatrix, instanceMatrix);
   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
-  gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+  gl.bindTexture(gl.TEXTURE_2D,TVPaper);
+  gl.drawArrays(gl.TRIANGLES, 24, 6);
 }
 
 function leftWall() {
   var s = scalem(1, 1, 1);
-  var instanceMatrix = mult(translate(-0.6, 0.0, 0.0), s);
+  var instanceMatrix = mult(translate(-0.8, 0.0, 0.0), s);
   instanceMatrix = mult(instanceMatrix, rotate(-270, 0, 1, 0));
   var t = mult(modelViewMatrix, instanceMatrix);
   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
+  gl.bindTexture(gl.TEXTURE_2D,wallPaper);
   gl.drawArrays(gl.TRIANGLES, 0, numVertices);
 }
 
 function rightWall() {
   var s = scalem(1, 1, 1);
-  var instanceMatrix = mult(translate(0.6, 0.0, 0.0), s);
+  var instanceMatrix = mult(translate(0.8, 0.0, 0.0), s);
   instanceMatrix = mult(instanceMatrix, rotate(-270, 0, 1, 0));
   var t = mult(modelViewMatrix, instanceMatrix);
   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
@@ -341,7 +366,7 @@ function printBase() {
 }
 
 function backWall() {
-  var s = scalem(1, 1, 1);
+  var s = scalem(1.8, 1, 1);
   var instanceMatrix = mult(translate(0.0, 0.0, 0.5), s);
   var t = mult(modelViewMatrix, instanceMatrix);
   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
@@ -349,9 +374,64 @@ function backWall() {
 }
 
 function holder() {
-  var s = scalem(0.2, 0.5, 0.2);
-  var instanceMatrix = mult(translate(0.0, -0.3, 0.0), s);
+  var s = scalem(0.6, 0.08, 1.5);
+  var instanceMatrix = mult(translate(0.0, -0.1, -0.3), s);
   var t = mult(modelViewMatrix, instanceMatrix);
   gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
+  gl.bindTexture(gl.TEXTURE_2D,metalPaper);
+  gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+}
+function backBoard(){
+  var s = scalem(1, 0.6, 0.8);
+  var instanceMatrix = mult(translate(0.0, 0.3, 0.18), s);
+  var t = mult(modelViewMatrix, instanceMatrix);
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
+  gl.bindTexture(gl.TEXTURE_2D,metalPaper);
+  gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+}
+function floor(){
+  var s = scalem(1.8, 1, 1);
+  var instanceMatrix = mult(translate(0.0, -0.6, 0.0), s);
+  instanceMatrix = mult(instanceMatrix,rotate(90,1,0,0));
+  var t = mult(modelViewMatrix, instanceMatrix);
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
+  gl.bindTexture(gl.TEXTURE_2D,floorPaper);
+  gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+}
+
+function table(){
+  var s = scalem(0.8, 0.5, 0.5);
+  var instanceMatrix = mult(translate(0.0, -0.2, -0.3), s);
+  instanceMatrix = mult(instanceMatrix,rotate(90,1,0,0));
+  var t = mult(modelViewMatrix, instanceMatrix);
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
+  gl.bindTexture(gl.TEXTURE_2D,holderPaper);
+  gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+}
+
+function tableLeg1(){
+  var s = scalem(0.1, 0.3, 1.5);
+  var instanceMatrix = mult(translate(0.3, -0.3, -0.3), s);
+  var t = mult(modelViewMatrix, instanceMatrix);
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
+  gl.bindTexture(gl.TEXTURE_2D,holderPaper);
+  gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+}
+
+function tableLeg2(){
+  var s = scalem(0.1, 0.3, 1.5);
+  var instanceMatrix = mult(translate(-0.3, -0.3, -0.3), s);
+  var t = mult(modelViewMatrix, instanceMatrix);
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
+  gl.bindTexture(gl.TEXTURE_2D,holderPaper);
+  gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+}
+
+function neck(){
+  var s = scalem(0.1, 0.3, 0.7);
+  var instanceMatrix = mult(translate(0.0, -0.1, 0.1), s);
+  var t = mult(modelViewMatrix, instanceMatrix);
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(t));
+  gl.bindTexture(gl.TEXTURE_2D,metalPaper);
   gl.drawArrays(gl.TRIANGLES, 0, numVertices);
 }
